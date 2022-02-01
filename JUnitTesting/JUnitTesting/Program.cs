e14f901e4c081751d7921c9e3bc48cb56d178e8c
Manish <manishkumar0156@gmail.com>
2022-02-01 14:06:01 +0530

    [Manish] Add: upload Vending Machine problem

---
+++ JUnitTesting/JUnitTesting/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUnitTesting
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1.to calculate the minimum number of notes \n");
+                    
+
+                bool flag = true;
+            while (flag)
+            {
+                int check = Convert.ToInt32(Console.ReadLine());
+
+                switch (check)
+                {
+                    case 1:
+                        VendingMachine machine = new VendingMachine();
+                        machine.Calculate();
+                        break;
+
+                }
+            }
+        }       
+    }
+}
---
+++ JUnitTesting/JUnitTesting/VendingMachine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JUnitTesting
+{
+    public class VendingMachine
+    {
+        public void Calculate()
+        {
+            int[] notes = { 1000, 500, 100, 50, 10, 5, 2, 1 };
+            int[] store = new int[notes.Length];
+            int change = 2006;
+            for(int i = 0; i < notes.Length; i++)
+            {
+                if (change/notes[i]>0) //2,1
+                {
+                    store[i] = change / notes[i];//store[0]=2
+                    change -= notes[i]*store[i];//2006-(1000*2)=6,1
+                }
+            }
+            for(int i = 0; i<notes.Length; i++)
+            {
+                if (store[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + "_" + store[i]);
+                }
+            }
+        }
+    }
+}
